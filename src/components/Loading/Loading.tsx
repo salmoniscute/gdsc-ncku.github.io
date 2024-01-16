30411c2e4eba6ef7dfa65428d1594795aae54be1
Jamie <rita60327@gmail.com>
2024-01-16 20:50:06 +0800

    Add loading page

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,15 @@
+import './Loading.scss'
+
+const Loading=()=>{
+  return(
+    <body className='background'>
+        <div className='back'>
+            <span className='type'>
+                National Cheng Kung University
+                Google Developer Student Club ...
+            </span>
+        </div>
+    </body>
+  )
+}
+export default Loading;
